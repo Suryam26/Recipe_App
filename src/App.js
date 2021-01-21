b2d61a8b61d193f5fcbcca8aeddedb5ca06efc05
Suryam26 <suryam.jain@gmail.com>
2021-01-21 08:46:53 +0530

    Added search-bar and recipe-cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '01df3c13';
+  const APP_KEY = '919fb065d16b4dc8acaf97fa75febdc1';
+  
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(()=>{
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
   );
 }
